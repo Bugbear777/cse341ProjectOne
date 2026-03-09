a7186c9ad026995e4af3431104734880ff0d57a3
Joseph-Marlow <jmarlow@byupathway.edu>
2026-03-08 18:29:38 -0700

    points on end check in

---
+++ data/database.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MongoClient = require('mongodb').MongoClient;
+let database;
+
+const intDb = (callback) => {
+    if (database) {
+        console.log('Database connection already established');
+        return callback(null, database);
+    }
+    MongoClient.connect(process.env.MONGODB_URI)
+        .then((client) => {
+            database = client;
+            callback(null, database);
+        })
+        .catch((err) => {
+            callback(err);
+        });
+};
+
+const getDatabase = () => {
+    if (!database) {
+        throw new Error('Database not initialized');
+    }
+    return database;
+};
+
+module.exports = {
+    initDb: intDb,
+    getDb: getDatabase,
+};
