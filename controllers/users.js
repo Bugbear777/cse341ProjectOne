c9c7a00d73475c15c4f9c44aae48a4e1cc104e70
Joseph-Marlow <jmarlow@byupathway.edu>
2026-03-13 15:02:55 -0700

    installed swagger and other project stuff

--- controllers/users.js
+++ controllers/users.js
@@ -17,7 +17,53 @@ const getSingle = async (req, res) => {
     res.status(200).json(result);
 };
 
+const createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db().collection('users').insertOne(user);
+    if(response.modifiedCount > 0) {
+        res.status(201).json(response);
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the user.');
+    }
+};
+
+const updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        username: req.body.username,
+        email: req.body.email,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
+    };
+    const response = await mongodb.getDb().db().collection('users').replaceOne({ _id: userId }, user);
+    if(response.acknowledged > 0) {
+        res.status(201).json(response);
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('users').deleteOne({ _id: userId });
+    if(response.deletedCount > 0) {
+        res.status(204).json(response);
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the user.');
+    }
+};
+
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const routes = require('express').Router();
 
+routes.use('/swagger', require('./swagger'));
+
 routes.get('/', (req, res) => {
   res.send('Hello, World!');
 });
